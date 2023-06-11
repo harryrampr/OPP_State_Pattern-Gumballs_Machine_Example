65d0cf2bbe5b3a7d24be0bf5811a58b3442ec8c2
Harry Ramirez <harryrampr@gmail.com>
2023-06-11 12:27:08 -0400

    Add documentation

---
+++ app/tests/GumBallMachineTest.php
@@ -0,0 +1,311 @@
+<?php /** @noinspection PhpExpressionResultUnusedInspection */
+
+namespace Tests;
+
+use App\GumBallMachine;
+use App\HasQuarterState;
+use App\NoQuarterState;
+use App\SoldOutState;
+use App\SoldState;
+use App\WinnerState;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class GumBallMachineTest extends TestCase
+{
+    /**
+     * Test that properties exist, have correct access, correct type, and correct default value.
+     */
+    public function testGumBallMachinePropertiesExist(): void
+    {
+        define('NO_DEFAULT_VALUE', 'NO_DEFAULT_VALUE');
+        $testObject = $this->createMock(GumBallMachine::class);
+        $reflection = new ReflectionClass(GumBallMachine::class);
+        $properties = $reflection->getProperties();
+
+        $expected = [
+            'soldOutState' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'noQuarterState' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'hasQuarterState' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'soldState' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'winnerState' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'state' => [
+                'access' => 'private',
+                'type' => 'App\State',
+                'default' => NO_DEFAULT_VALUE,
+            ],
+            'count' => [
+                'access' => 'private',
+                'type' => 'int',
+                'default' => 0,
+            ],
+        ];
+
+        foreach ($properties as $property) {
+            $name = $property->getName();
+            $this->assertArrayHasKey($name, $expected);
+
+            // Test access type
+            match ($expected[$name]['access']) {
+                'public' => $this->assertTrue($property->isPublic()),
+                'protected' => $this->assertTrue($property->isProtected()),
+                'private' => $this->assertTrue($property->isPrivate()),
+                default => $this->fail('Unexpected access type'),
+            };
+
+            // Test type
+            $this->assertSame($expected[$name]['type'],
+                $property->getType()->getName());
+
+            // Test default value
+            $property->setAccessible(true);
+            $this->assertSame($expected[$name]['default'],
+                $property->hasDefaultValue() ? $property->getValue($testObject) : NO_DEFAULT_VALUE);
+        }
+    }
+
+    /**
+     * Test that methods exist, have correct access, and correct return type.
+     */
+    public function testGumBallMachineMethodsExist(): void
+    {
+        $reflection = new ReflectionClass(GumBallMachine::class);
+        $methods = $reflection->getMethods();
+        $expected = [
+            '__construct' => [
+                'access' => 'public',
+                'return' => '',
+            ],
+            '__toString' => [
+                'access' => 'public',
+                'return' => 'string',
+            ],
+            'insertQuarter' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'ejectQuarter' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'turnCrank' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'dispense' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'releaseBall' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'getCount' => [
+                'access' => 'public',
+                'return' => 'int',
+            ],
+            'refill' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'getState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+            'setState' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'setCount' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'getSoldOutState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+            'getNoQuarterState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+            'getHasQuarterState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+            'getSoldState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+            'getWinnerState' => [
+                'access' => 'public',
+                'return' => 'App\State',
+            ],
+        ];
+
+        foreach ($methods as $method) {
+            $name = $method->getName();
+            $this->assertArrayHasKey($name, $expected);
+
+            // Match access type, but not return type
+            match ($expected[$name]['access']) {
+                'public' => $this->assertTrue($method->isPublic()),
+                'protected' => $this->assertTrue($method->isProtected()),
+                'private' => $this->assertTrue($method->isPrivate()),
+                default => $this->fail('Unexpected access type'),
+            };
+
+            $this->assertSame($expected[$name]['return'], $method->hasReturnType() ? $method->getReturnType()->getName() : '');
+        }
+    }
+
+    /**
+     * Test the GumBallMachine constructor.
+     */
+    public function testGumBallMachineConstruct(): void
+    {
+        // Test that the constructor sets the state to NoQuarterState if the count > 0
+        $gumBallMachine1 = new GumBallMachine(5);
+        $this->assertSame(5, $gumBallMachine1->getCount());
+        $this->assertInstanceOf(NoQuarterState::class, $gumBallMachine1->getState());
+
+        // Test that the constructor sets other states and getters work
+        $this->assertInstanceOf(SoldOutState::class, $gumBallMachine1->getSoldOutState());
+        $this->assertInstanceOf(HasQuarterState::class, $gumBallMachine1->getHasQuarterState());
+        $this->assertInstanceOf(SoldState::class, $gumBallMachine1->getSoldState());
+        $this->assertInstanceOf(WinnerState::class, $gumBallMachine1->getWinnerState());
+
+        // Test that the constructor sets the state to SoldOutState if the count is 0
+        $gumBallMachine2 = new GumBallMachine(0);
+        $this->assertSame(0, $gumBallMachine2->getCount());
+        $this->assertInstanceOf(SoldOutState::class, $gumBallMachine2->getState());
+    }
+
+    /**
+     * Test the __toString method of GumBallMachine.
+     */
+    public function testGumBallMachineToString(): void
+    {
+        $testCount = 5;
+        $gumBallMachine = new GumBallMachine($testCount);
+        $expected = '<div class="monitor"><h1>Mighty Gumball, Inc.</h1>' . PHP_EOL;
+        $expected .= '<h2>Java-enabled Standing Gumball Model #2004</h2>' . PHP_EOL;
+        $expected .= "<p>Inventory: $testCount gumballs</p>" . PHP_EOL;
+        $expected .= '<p class="message">Machine is waiting for quarter</p></div>' . PHP_EOL;
+
+        $this->assertSame($expected, $gumBallMachine->__toString());
+    }
+
+    /**
+     * Test the insertQuarter method of GumBallMachine.
+     */
+    public function testGumBallMachineInsertQuarter(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $gumBallMachine->insertQuarter();
+        $this->assertInstanceOf(HasQuarterState::class, $gumBallMachine->getState());
+    }
+
+    /**
+     * Test the ejectQuarter method of GumBallMachine.
+     */
+    public function testGumBallMachineEjectQuarter(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $gumBallMachine->insertQuarter();
+        $gumBallMachine->ejectQuarter();
+        $this->assertInstanceOf(NoQuarterState::class, $gumBallMachine->getState());
+    }
+
+    /**
+     * Test the turnCrank method of GumBallMachine.
+     */
+    public function testGumBallMachineTurnCrank(): void
+    {
+        // Test that the state is an instance of NoQuarterState
+        $gumBallMachine1 = new GumBallMachine(5);
+        $gumBallMachine1->insertQuarter();
+        $gumBallMachine1->turnCrank();
+        $this->assertInstanceOf(NoQuarterState::class, $gumBallMachine1->getState());
+
+        // Test that the state is an instance of SoldOutState
+        $gumBallMachine2 = new GumBallMachine(1);
+        $gumBallMachine2->insertQuarter();
+        $gumBallMachine2->turnCrank();
+        $this->assertInstanceOf(SoldOutState::class, $gumBallMachine2->getState());
+    }
+
+    /**
+     * Test the setState method of GumBallMachine.
+     */
+    public function testGumBallMachineSetState(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $gumBallMachine->setState(new SoldOutState($gumBallMachine));
+        $this->assertInstanceOf(SoldOutState::class, $gumBallMachine->getState());
+    }
+
+    /**
+     * Test the setCount method of GumBallMachine.
+     */
+    public function testGumBallMachineSetCount(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $gumBallMachine->setCount(10);
+        $this->assertSame(10, $gumBallMachine->getCount());
+    }
+
+    /**
+     * Test the releaseBall method of GumBallMachine.
+     */
+    public function testGumBallMachineReleaseBall(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $this->expectOutputString('<p class="action">A gumball comes rolling out the slot</p>' .
+            PHP_EOL);
+        $gumBallMachine->releaseBall();
+        $this->assertSame(4, $gumBallMachine->getCount());
+    }
+
+    /**
+     * Test the getCount method of GumBallMachine.
+     */
+    public function testGumBallMachineGetCount(): void
+    {
+        $testCount = 5;
+        $gumBallMachine = new GumBallMachine($testCount);
+        $this->assertSame($testCount, $gumBallMachine->getCount());
+    }
+
+    /**
+     * Test the refill method of GumBallMachine.
+     */
+    public function testGumBallMachineRefill(): void
+    {
+        $gumBallMachine = new GumBallMachine(5);
+        $this->assertSame(5, $gumBallMachine->getCount());
+
+        $gumBallMachine->refill(10);
+        $this->assertSame(15, $gumBallMachine->getCount());
+        $this->assertInstanceOf(NoQuarterState::class, $gumBallMachine->getState());
+    }
+}
---
+++ app/tests/HasQuarterStateTest.php
@@ -0,0 +1,218 @@
+<?php /** @noinspection PhpExpressionResultUnusedInspection */
+
+namespace Tests;
+
+use App\GumBallMachine;
+use App\HasQuarterState;
+use App\SoldState;
+use App\State;
+use App\WinnerState;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+/**
+ * Class HasQuarterStateTest
+ *
+ * @package Your\Namespace
+ */
+class HasQuarterStateTest extends TestCase
+{
+    /**
+     * Tests that HasQuarterState implements State using reflection.
+     */
+    public function testHasQuarterStateImplementsState(): void
+    {
+        $hasQuarterStateReflection = new ReflectionClass(HasQuarterState::class);
+        $this->assertTrue($hasQuarterStateReflection->implementsInterface(State::class));
+    }
+
+    /**
+     * Tests that properties exist, have the correct access modifiers and types.
+     */
+    public function testHasQuarterStatePropertiesExist(): void
+    {
+        $reflection = new ReflectionClass(HasQuarterState::class);
+        $properties = $reflection->getProperties();
+        $expected = [
+            'gumBallMachine' => [
+                'access' => 'private',
+                'type' => 'App\GumBallMachine',
+            ],
+        ];
+
+        foreach ($properties as $property) {
+            $name = $property->getName();
+            $this->assertArrayHasKey($name, $expected);
+
+            // Match access type
+            match ($expected[$name]['access']) {
+                'public' => $this->assertTrue($property->isPublic()),
+                'protected' => $this->assertTrue($property->isProtected()),
+                'private' => $this->assertTrue($property->isPrivate()),
+                default => $this->fail('Unexpected access type'),
+            };
+
+            $this->assertSame($expected[$name]['type'], $property->hasType() ?
+                $property->getType()->getName() : '');
+
+        }
+    }
+
+    /**
+     * Tests that the insertQuarter method exists and has the correct access modifier and return type.
+     */
+    public function testHasQuarterStateMethodsExists(): void
+    {
+        $reflection = new ReflectionClass(HasQuarterState::class);
+        $methods = $reflection->getMethods();
+        $expected = [
+            '__construct' => [
+                'access' => 'public',
+                'return' => '',
+            ],
+            'insertQuarter' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'ejectQuarter' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'turnCrank' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            'dispense' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+            '__toString' => [
+                'access' => 'public',
+                'return' => 'string',
+            ],
+            'refill' => [
+                'access' => 'public',
+                'return' => 'void',
+            ],
+        ];
+
+        foreach ($methods as $method) {
+            $name = $method->getName();
+            $this->assertArrayHasKey($name, $expected);
+
+            // Test access type
+            match ($expected[$name]['access']) {
+                'public' => $this->assertTrue($method->isPublic()),
+                'protected' => $this->assertTrue($method->isProtected()),
+                'private' => $this->assertTrue($method->isPrivate()),
+                default => $this->fail('Unexpected access type'),
+            };
+
+            // Test return type
+            $this->assertSame($expected[$name]['return'], $method->hasReturnType() ?
+                $method->getReturnType()->getName() : '');
+        }
+    }
+
+    /**
+     * Tests that the constructor sets the gumBallMachine property.
+     */
+    public function testHasQuarterStateConstructSetsGumBallMachine(): void
+    {
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $reflection = new ReflectionClass(HasQuarterState::class);
+        $property = $reflection->getProperty('gumBallMachine');
+        $property->setAccessible(true);
+
+        $this->assertSame($gumBallMachine, $property->getValue($hasQuarterState));
+    }
+
+    /**
+     * Tests that the insertQuarter method outputs the expected message.
+     */
+    public function testHasQuarterStateInsertQuarterOutputsInsertQuarterMessage(): void
+    {
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $this->expectOutputString('<p class="action">You can’t insert another quarter</p>' . PHP_EOL);
+        $hasQuarterState->insertQuarter();
+    }
+
+    /**
+     * Tests that the ejectQuarter method outputs the expected message.
+     */
+    public function testHasQuarterStateEjectQuarterOutputsEjectQuarterMessage(): void
+    {
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $this->expectOutputString('<p class="action">Quarter returned</p>' . PHP_EOL);
+        $hasQuarterState->ejectQuarter();
+    }
+
+    /**
+     * Tests that the turnCrank method outputs the expected message and sets the state.
+     */
+    public function testHasQuarterStateTurnCrankOutputsTurnCrankMessageAndSetsState(): void
+    {
+        $testTimes = 100;
+        $expectedOutput = '<p class="action">You turned</p>' . PHP_EOL;
+
+        $gumBallMachineMock = $this->createMock(GumBallMachine::class);
+        $gumBallMachineMock->expects($this->atLeast(1))->method('getCount')->willReturn($testTimes * 2);
+        // SoldState must be called at least 75% of the time
+        $gumBallMachineMock->expects($this->atLeast(ceil($testTimes * .75)))->method('getSoldState');
+
+        // getSoldState must be called at least 4% of the time
+        $gumBallMachineMock->expects($this->atLeast(ceil($testTimes * .04)))->method('getWinnerState');
+
+        $hasQuarterState = new HasQuarterState($gumBallMachineMock);
+
+        for ($i = 0; $i < $testTimes; $i++) {
+            ob_start();
+            $hasQuarterState->turnCrank();
+            $output = ob_get_clean();
+            $this->assertSame($expectedOutput, $output);
+        }
+    }
+
+    /**
+     * Tests that the dispense method outputs the expected message.
+     */
+    public function testHasQuarterStateDispenseOutputsDispenseMessage(): void
+    {
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $this->expectOutputString('<p class="action">No gumball dispensed</p>' . PHP_EOL);
+        $hasQuarterState->dispense();
+    }
+
+    /**
+     * Tests that the __toString method returns the expected output.
+     */
+    public function testHasQuarterStateToStringOutput(): void
+    {
+        $expectedOutput = 'Machine has a quarter';
+
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $this->assertSame($expectedOutput, (string)$hasQuarterState);
+    }
+
+    /**
+     * Tests that the refill method outputs the expected message.
+     */
+    public function testHasQuarterStateRefill(): void
+    {
+        $gumBallMachine = $this->createMock(GumBallMachine::class);
+        $hasQuarterState = new HasQuarterState($gumBallMachine);
+
+        $this->expectOutputString("<p class=\"action\">Can't refill now</p>" . PHP_EOL);
+        $hasQuarterState->refill(10);
+    }
+}
